24696001b66cfa01cbcc640b24485486c0e6af8f
Jeff Meunier <jeffrey.a.meunier@gmail.com>
2025-05-13 04:21:13 -0500

    Added Real literal type.

---
+++ UFO/src/Types/Literal/Real.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace UFO.Types.Literal;
+
+public class Real : Literal
+{
+
+    private static readonly int HashSeed = typeof(Real).GetHashCode();
+
+    private readonly double Value;
+
+    private Real(double value)
+    {
+        Value = value;
+    }
+
+    public static Real Create(double value)
+    {
+        return new Real(value);
+    }
+
+    public override bool BoolValue()
+    {
+        return Value != 0.0;
+    }
+
+    public override bool EqualsAux([NotNull] UFOObject otherObj)
+    {
+        Real other = (Real)otherObj;
+        return Value == other.Value;
+    }
+
+    public override int GetHashCode()
+    {
+        return Utils.Hash.CombineHash(HashSeed, Value.GetHashCode());
+    }
+    
+    public override void ToString(StringBuilder sb)
+    {
+        sb.Append(Value);
+    }
+
+}
